076cd7a1112933bce1dbfabe14565a1908576b78
EtecPaiva <sungjinwoo.rankss@gmail.com>
2026-05-14 11:41:15 +0000

    começo_do_Bah2

--- src/core/middlewares/error.middleware.js
+++ src/core/middlewares/error.middleware.js
@@ -2,26 +2,63 @@ import * as responses from '../utils/responses.js';
 import { logError } from '../utils/logger.js';
 
 export const errorMiddleware = (error, req, res, next) => {
-  const status = error.code || 500;
-  const message = error.message || 'Erro interno do servidor';
-  const reason = error.reason || '';
+  const status = Number(error.code) || 500;
 
-  logError({ error, req, status, message });
+  const message = error.message || 'Erro interno do servidor';
+
+  const details = error.reason || error.stack || '';
+
+  logError({
+    error,
+    req,
+    status,
+    message
+  });
 
   switch (status) {
     case 400:
-      return responses.badRequest(res, { message, error: reason });
+      return responses.badRequest(res, {
+        message,
+        details
+      });
+
     case 401:
-      return responses.unauthorized(res, { message, error:reason });
+      return responses.unauthorized(res, {
+        message,
+        details
+      });
+
     case 403:
-      return responses.forbidden(res, { message, error: reason });
+      return responses.forbidden(res, {
+        message,
+        details
+      });
+
     case 404:
-      return responses.notFound(res, { message, error: reason });
+      return responses.notFound(res, {
+        message,
+        details
+      });
+
     case 409:
-      return responses.conflict(res, { message, error: reason });
+      return responses.conflict(res, {
+        message,
+        details
+      });
+
     case 498:
-      return responses.invalidToken(res, { message, error: reason });
+      return responses.invalidToken(res, {
+        message,
+        details
+      });
+
     default:
-      return responses.error(res, { status, message, error: reason });
+      return responses.error(res, {
+        status,
+        message,
+        details
+      });
   }
 };
+
+// code podia vir como string, Não havia padronização do campo técnico.
